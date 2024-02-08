e53f0030f9012c53b89973d853013e2cae80e67f
John Sexton <john.a.sexton@gmail.com>
2024-02-08 11:25:32 -0500

    making filenames more relevant

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# generative-ai
+### generative-ai
+
+##### Various ideas for use with Vertex-AI in GCP
+* main.py - one shot genAI prompt
+* codey-chat.py - use codey api for code generation
+* chirp-transcription.py - transcribe speech 2 text w/ chirp api
+* hardcodeddta.py - testing inline BLOB as audio_data 
--- codey-chat.py
+++ codey-chat.py
@@ -39,21 +39,4 @@ generation_model = TextGenerationModel.from_pretrained("text-bison@001")
 
 # assumes: from vertexai.language_models import CodeChatModel
 
-def write_a_function(temperature: float = 0.5) -> object:
-    """Example of using Codey for Code Chat Model to write a function."""
-
-    # TODO developer - override these parameters as needed:
-    parameters = {
-        "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
-        "max_output_tokens": 1024,  # Token limit determines the maximum amount of text output.
-    }
-
-    # assumes: code_chat_model = CodeChatModel.from_pretrained("codechat-bison@001")
-    chat = code_chat_model.start_chat()
-
-    response = chat.send_message(
-        "Please help write a function to calculate the min of two numbers", **parameters
-    )
-    print(f"Response from Model: {response.text}")
-
-    return response
+s
